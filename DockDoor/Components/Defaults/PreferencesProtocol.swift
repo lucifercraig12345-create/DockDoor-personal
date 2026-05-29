625f0e3a4abc3842bbe95fa4197a9e2f870f9b73
Lucifer <lucifercraig12345@gmail.com>
2026-05-29 06:29:35 +0530

    Update PreferencesProtocol.swift

--- DockDoor/Components/Defaults/PreferencesProtocol.swift
+++ DockDoor/Components/Defaults/PreferencesProtocol.swift
@@ -2,3 +2,8 @@ import Defaults
 import SwiftUI
 
 protocol Preferences: Equatable, Hashable, Codable, Defaults.Serializable {}
+
+// --- ADDED: The memory key for our new Three Finger Swipe setting ---
+extension Defaults.Keys {
+    static let enableWindowSwitcherSwipe = Key<Bool>("enableWindowSwitcherSwipe", default: true)
+}
